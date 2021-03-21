813ae466ce3d2ea9bd52357b7bba80f986acaab9
Sauce101 <michaelsaucedo443@gmail.com>
2021-03-21 15:10:29 -0700

    initialcommit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navlist = document.getElementById('nav-list')
+
+// function toggleButton() {
+//     navlist.classList.toggle('show');
+// }
+
+toggleButton = () => navlist.classList.toggle('show');
+
+hamburgerButton.addEventListener('click', toggleButton)
