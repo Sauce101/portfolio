e470647bb0383269722eb24fca0968ab52e9d232
Sauce101 <michaelsaucedo443@gmail.com>
2021-11-27 01:13:19 -0800

    education link and font change

--- src/app.js
+++ src/app.js
@@ -1,31 +1,5 @@
-// const hamburgerButton = document.getElementById('hamburger');
-// const navlist = document.getElementById('nav-list');
 
-// function toggleButton() {
-//     navlist.classList.toggle('show');
-// }
-
-// toggleButton = () => navlist.classList.toggle('show');
-
-// hamburgerButton.addEventListener('click', toggleButton);
-
-
-// ham
-// $(document).ready(function(){
-//   $('#nav-icon1').click(function(){
-//     $(this).toggleClass('open');
-//   });
-// });
-
-// this works as just the burger
-// const hamburgerButton = document.getElementById('nav-icon1');
-
-// toggleButton = () => hamburgerButton.classList.toggle('open');
-
-// hamburgerButton.addEventListener('click', toggleButton);
-
-// try
-const hamburgerButton = document.getElementById('nav-icon1');
+const hamburgerButton = document.getElementById('hamburger');
 const navlist = document.getElementById('nav-list');
 
 toggleButton = () => {
@@ -34,4 +8,3 @@ toggleButton = () => {
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
-
