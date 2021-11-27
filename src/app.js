7000e6253e7411e117717764420141bff0f45d94
Sauce101 <michaelsaucedo443@gmail.com>
2021-11-26 22:30:42 -0800

    new hamburger animation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navlist = document.getElementById('nav-list')
+// const hamburgerButton = document.getElementById('hamburger');
+// const navlist = document.getElementById('nav-list');
 
 // function toggleButton() {
 //     navlist.classList.toggle('show');
 // }
 
-toggleButton = () => navlist.classList.toggle('show');
+// toggleButton = () => navlist.classList.toggle('show');
 
-hamburgerButton.addEventListener('click', toggleButton)
+// hamburgerButton.addEventListener('click', toggleButton);
+
+
+// ham
+// $(document).ready(function(){
+//   $('#nav-icon1').click(function(){
+//     $(this).toggleClass('open');
+//   });
+// });
+
+// this works as just the burger
+// const hamburgerButton = document.getElementById('nav-icon1');
+
+// toggleButton = () => hamburgerButton.classList.toggle('open');
+
+// hamburgerButton.addEventListener('click', toggleButton);
+
+// try
+const hamburgerButton = document.getElementById('nav-icon1');
+const navlist = document.getElementById('nav-list');
+
+toggleButton = () => {
+  hamburgerButton.classList.toggle('open')
+  navlist.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton);
+
