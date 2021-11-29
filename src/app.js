91582b628939e8285658218d3aebffb0994ce974
Sauce101 <michaelsaucedo443@gmail.com>
2021-11-28 20:11:37 -0800

    added smooth-scroll to nav-list

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-
-const hamburgerButton = document.getElementById('hamburger');
-const navlist = document.getElementById('nav-list');
+// hamburger animate
+const hamburgerButton = document.getElementById("hamburger");
+const navlist = document.getElementById("nav-list");
 
 toggleButton = () => {
-  hamburgerButton.classList.toggle('open')
-  navlist.classList.toggle('show')
-}
+  hamburgerButton.classList.toggle("open");
+  navlist.classList.toggle("show");
+};
 
-hamburgerButton.addEventListener('click', toggleButton);
+hamburgerButton.addEventListener("click", toggleButton);
+
+// smooth-scroll
+const scroll = new SmoothScroll('#nav-list a[href*="#"]', {
+  speed: 800,
+});
